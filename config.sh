71517abc6f82af3bd33bc4c668326ccf992614c3
exalromansy1 <120054209+exalromansy1@users.noreply.github.com>
2023-01-03 17:34:17 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,14 +4,14 @@
 export FOX_BRANCH="fox_9.0"
 export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="Infinix-X626B"
+export OEM="infinix"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
 ## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
 ## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
-export TARGET="recoveryimage"
+export TARGET="bootimage"
 
 export OUTPUT="OrangeFox*.zip"
 
@@ -31,6 +31,6 @@ export OF_USE_LATEST_MAGISK=true
 # Not Recommended to Change
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
 export USE_CCACHE=1
-export CCACHE_SIZE="50G"
+export CCACHE_SIZE="20G"
 export CCACHE_DIR="$HOME/work/.ccache"
 export J_VAL=16
